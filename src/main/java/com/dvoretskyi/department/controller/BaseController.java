e92e7c272abb25b1d6a57bf842f77606c2f8ca7f
OrestDvoretskyi <Or4yk5@gmail.com>
2018-11-11 01:08:36 +0200

    add changes

--- src/main/java/com/dvoretskyi/department/controller/BaseController.java
+++ src/main/java/com/dvoretskyi/department/controller/BaseController.java
@@ -1,11 +1,19 @@
 package com.dvoretskyi.department.controller;
 
-import com.dvoretskyi.department.services.DepartmentService;
-import com.dvoretskyi.department.services.EmployeeService;
+import com.dvoretskyi.department.entity.Employee;
+import com.dvoretskyi.department.services.impl.DepartmentServiceImpl;
+import com.dvoretskyi.department.services.impl.EmployeeServiceImpl;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * The type Base controller.
@@ -13,22 +21,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BaseController {
 
-  @Autowired
-  private EmployeeService employeeService;
+  private static int currentPage = 1;
+  private static int pageSize = 5;
 
   @Autowired
-  private DepartmentService departmentService;
+  private EmployeeServiceImpl employeeService;
 
-  /**
-   * List employees string.
-   *
-   * @param model the model
-   * @return the string
-   */
+  @Autowired
+  private DepartmentServiceImpl departmentService;
+
   @GetMapping("/")
-  public String listEmployees(Model model) {
-    model.addAttribute("employees", employeeService.findAllEmployees());
+  public String listEmployees(Model model,
+      @RequestParam("page") Optional<Integer> page,
+      @RequestParam("size") Optional<Integer> size) {
+    page.ifPresent(p -> currentPage = p);
+    size.ifPresent(s -> pageSize = s);
+
+    Page<Employee> employeePage = employeeService.findPaginated(PageRequest
+        .of(currentPage - 1, pageSize));
+
+    model.addAttribute("employeePage", employeePage);
+
+    int totalPages = employeePage.getTotalPages();
+    if (totalPages > 0) {
+      List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+          .boxed()
+          .collect(Collectors.toList());
+      model.addAttribute("pageNumbers", pageNumbers);
+    }
+    model.addAttribute("employees", employeePage.getContent());
     model.addAttribute("departments", departmentService.findAllDepartments());
     return "index";
   }
+
 }
